3846cbb274ce1099120c94f4592314dd610b1994
Gabriel <gabrielbdf@hotmail.com>
2024-01-11 14:11:26 +0000

    add texts for backend

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,15 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
+  name: "Gabriel Gomes",
+  title: "Programador Backend, Web e Mobile",
+  email: "gabrielbdf@hotmail.com",
+  gitHub: "https://github.com/gabrielbdf",
+  instagram: "",
+  linkedIn: "https://www.linkedin.com/in/gabrielgomes1/",
   medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  twitter: "",
+  youTube: "",
 };
 
 const primaryColor = "#4E567E";
--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -24,19 +24,19 @@ const imageAltText = "purple and blue abstract background";
  * Sort description that expands on your title on the Home component.
  */
 const description =
-  "I'm a UI/UX student studying at Barnett Technical University. I enjoy creating unique and simplistic user interfaces in creative ways.";
+  "I'm someone who works on making computer things run smoothly. I enjoy creating simple and special ways for people to use computer programs.";
 
 /**
  * List of some of skills or technologies you work on, are learning,
  * passionate about, or enjoy,
  */
 const skillsList = [
-  "Web design",
-  "User experience",
-  "Inclusive design",
-  "Focus group testing",
-  "Mobile user interfaces",
-  "Graphic design",
+  "Web development",
+  "Backend programming",
+  "Database management",
+  "API design",
+  "Server maintenance",
+  "Algorithm implementation",
 ];
 
 /**
@@ -45,7 +45,7 @@ const skillsList = [
  * about you on a professional level.
  */
 const detailOrQuote =
-  "I am passionate about solving problems in new creative ways to drive innovation. By leveraging my UI/UX experience I continually look for new and better ways to make tech accessible by all.";
+  "I love finding inventive solutions to problems in the backend, driving innovation. Using my experience in making user interfaces and experiences, I always seek fresh and improved methods to make technology accessible to everyone.";
 
 const About = () => {
   return (
